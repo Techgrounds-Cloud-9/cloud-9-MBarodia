9713eabe316ca831ee397603c8500888ef7594c4
MBarodia <114392588+MBarodia@users.noreply.github.com>
2022-10-18 15:26:05 +0200

    Updated Comments

--- 04_Python/Scripts/PRG07-EXC01.py
+++ 04_Python/Scripts/PRG07-EXC01.py
@@ -1,4 +1,6 @@
-
+# Create a new script.
+# Create a variable that contains a list of five names.
+# Loop over the list using a for loop. Print every individual name in the list on a new line.
 
 arr = ('Á','B','C','D','E')
 for i in arr:
--- 04_Python/Scripts/PRG07-EXC02.py
+++ 04_Python/Scripts/PRG07-EXC02.py
@@ -1,4 +1,8 @@
 
+#Create a list of five integers.
+#Use a for loop to do the following for every item in the list:
+#Print the value of that item added to the value of the next item in the list.
+#If it is the last item, add it to the value of the first item instead (since there is no next item).
 
 lst = (5,10,15,20,25)
 num = range(len(lst))
