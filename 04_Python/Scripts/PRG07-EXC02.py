da566e34e9f3d90d3bb56ea52ab7407d4422a4bc
MBarodia <114392588+MBarodia@users.noreply.github.com>
2022-10-18 15:21:57 +0200

    New

--- 04_Python/Scripts/PRG07-EXC02.py
+++ 04_Python/Scripts/PRG07-EXC02.py
@@ -1,7 +1,20 @@
 
 
-arr = (10,20,30,40,50)
-for i in arr:
+lst = (5,10,15,20,25)
+num = range(len(lst))
+print(num)
+
+for i in num:
     print(i)
+    print(i+1)
+    print(lst[i])
+
+    if i < (len(lst)-1):
+        print(lst[i]+lst[i+1])
+    else:
+        print(lst[i]+lst[0])
+        break
+
     
 
+ 
